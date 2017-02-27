afa384d41188633954f8729e23d2c1ea56534cc9
Eric Powell <eric@Erics-MacBook-Air.local>
2017-02-27 10:18:47 -0700

    Modified site helpers to use current_page variable and moved get_product_url function.

--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,23 +1,22 @@
 module SiteHelpers
-  require 'net/smtp'
 
   # Used to check if a link goes to the current page by passing in a nav and
   # subnav string to check against the frontmatter within data.page.
   def current?(nav, subnav = "")
     if subnav.empty?
-      nav == data.page.nav
+      nav == current_page.data.nav
     else
-      nav == data.page.nav && subnav == data.page.subnav
+      nav == current_page.data.nav && subnav == current_page.data.subnav
     end
   end
 
   def page_title
-    (data.page.title.blank? ? '' : "#{data.page.title} - ") + project_setting(:title)
+    (current_page.data.title.blank? ? '' : "#{current_page.data.title} - ") + project_setting(:title)
   end
 
   # Creates a description meta tag based on the presence of a description value within the page frontmatter.
   def page_description
-    content_tag :meta, "", {name: "description", value: data.page.description } if data.page.description
+    content_tag :meta, "", {name: "description", value: current_page.data.description } if current_page.data.description
   end
 
   # Generates character length lorem ipsum strings
@@ -39,36 +38,43 @@ module SiteHelpers
     data.config[config[:environment]][key]
   end
 
+  # create url from product name (e.g. tom's soap => toms-soap)
+  def get_product_url(type, product)
+    "/products/#{type}/#{product['name'].gsub(/\'/, "").parameterize}.html"
+  end
+
+  #require 'net/smtp'
+  
   # Send prototyped transactional emails.
-  def send_mail(template, opts={})
-    if development?
-      begin
-        to      = opts[:to] || project_setting(:mail_to)
-        from    = project_setting(:mail_from)
-        subject = opts[:subject] || "(No Subject)"
-        content = File.read(File.join(root, "mailers", "#{template}.html"))
-        host    = project_setting(:mail_host)
-        port    = project_setting(:mail_port)
-        message = build_message(to, from, subject, content)
+  #def send_mail(template, opts={})
+    #if development?
+      #begin
+        #to      = opts[:to] || project_setting(:mail_to)
+        #from    = project_setting(:mail_from)
+        #subject = opts[:subject] || "(No Subject)"
+        #content = File.read(File.join(root, "mailers", "#{template}.html"))
+        #host    = project_setting(:mail_host)
+        #port    = project_setting(:mail_port)
+        #message = build_message(to, from, subject, content)
 
-        # If you need to customize this further you can use the following into the start method
-        # (host, port, domain, username, password :plain)
-        Net::SMTP.start(host, port) do |smtp|
-          smtp.sendmail(message, from, [to])
-        end
-      rescue
-        false
-      end
-    else
-      true
-    end
-  end
+        ## If you need to customize this further you can use the following into the start method
+        ## (host, port, domain, username, password :plain)
+        #Net::SMTP.start(host, port) do |smtp|
+          #smtp.sendmail(message, from, [to])
+        #end
+      #rescue
+        #false
+      #end
+    #else
+      #true
+    #end
+  #end
 
-  private
+  #private
 
-  # Returns a string to send as the mail message
-  def build_message(to, from, subject, body)
-    "From: #{from}\nTo: #{to}\nContent-type: text/html\nSubject: #{subject}\n\n#{body}"
-  end
+  ## Returns a string to send as the mail message
+  #def build_message(to, from, subject, body)
+    #"From: #{from}\nTo: #{to}\nContent-type: text/html\nSubject: #{subject}\n\n#{body}"
+  #end
 
 end
